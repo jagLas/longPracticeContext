aa531060a25d579fc56bc81df6112d1c04b4b054
jagLas <jglasher@gmail.com>
2022-12-08 08:56:16 -0500

    finish 5

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,5 +1,15 @@
 function changeContext(func, obj) {
-  // Your code here
+  return func.call(obj);
+}
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
 }
 
 /*****************************************************************************/
